fc509e1b9e5ccee636c75efa3624f51ad3963386
Gina Marieges <gina.marieges@gmail.com>
2025-12-27 17:27:52 +0100

    Create DirectionTest

--- tests/Unit/Domain/Rover/DirectionTest.php
+++ tests/Unit/Domain/Rover/DirectionTest.php
@@ -1,20 +1,23 @@
 <?php
 
+use App\Domain\Rover\ValueObjects\Position;
+use App\Domain\Rover\ValueObjects\Direction;
+
 describe('Given a Direction', function () {
     describe('When the turnLeft method is called', function () {
         test('Then it should rotate counter-clockwise', function () {
-            expect(Direction::N->turnLeft()->toBe(Direction::W));
-            expect(Direction::W->turnLeft()->toBe(Direction::S));
-            expect(Direction::S->turnLeft()->toBe(Direction::E));
-            expect(Direction::E->turnLeft()->toBe(Direction::N));
+            expect(Direction::N->turnLeft())->toBe(Direction::W);
+            expect(Direction::W->turnLeft())->toBe(Direction::S);
+            expect(Direction::S->turnLeft())->toBe(Direction::E);
+            expect(Direction::E->turnLeft())->toBe(Direction::N);
         });
     });
     describe('When the turnRight method is called', function () {
         test('Then it should rotate clockwise', function () {
-            expect(Direction::N->turnRight()->toBe(Direction::E));
-            expect(Direction::E->turnRight()->toBe(Direction::S));
-            expect(Direction::S->turnRight()->toBe(Direction::W));
-            expect(Direction::W->turnRight()->toBe(Direction::N));
+            expect(Direction::N->turnRight())->toBe(Direction::E);
+            expect(Direction::E->turnRight())->toBe(Direction::S);
+            expect(Direction::S->turnRight())->toBe(Direction::W);
+            expect(Direction::W->turnRight())->toBe(Direction::N);
         });
     });
     describe('When the nextPosition method is called with a position', function () {
